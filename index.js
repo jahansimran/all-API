aedab4026cbf74718db3e4ce7d1f734526cc7e2d
Jahanzeb <jahansimran244@gmail.com>
2022-08-10 09:36:11 +0000

    All Apis

---
+++ index.js
@@ -0,0 +1,153 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+const dbPath = path.join(__dirname, "goodreads.db");
+app.use(express.json());
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(4000, () => {
+      console.log("Server Running at http://localhost:4000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+// Get Books API
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+    SELECT
+      *
+    FROM
+      book
+    ORDER BY
+      book_id;`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
+
+//Get Book API
+app.get("/books/:bookId/", async (request, response) => {
+  const { bookId } = request.params;
+  const getOneBook = `
+     SELECT *
+     FROM book 
+     WHERE book_id = ${bookId};`;
+
+  const book = await db.get(getOneBook);
+  response.send(book);
+});
+
+//Add Book API
+app.post("/books/", async (request, response) => {
+  const bookDetails = request.body;
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails;
+  const addBookQuery = `
+    INSERT INTO
+      book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+    VALUES
+      (
+        '${title}',
+         ${authorId},
+         ${rating},
+         ${ratingCount},
+         ${reviewCount},
+        '${description}',
+         ${pages},
+        '${dateOfPublication}',
+        '${editionLanguage}',
+         ${price},
+        '${onlineStores}'
+      );`;
+  const dbResponse = await db.run(addBookQuery);
+  const bookId = dbResponse.lastID;
+  response.send({ bookId: bookId });
+});
+
+//Update Book API
+app.put("/books/:bookId", async (request, response) => {
+  const { bookId } = request.params;
+  const bookDetails = request.body;
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails;
+  const updateBookQuery = `
+    UPDATE
+      book
+    SET
+      title='${title}',
+      author_id=${authorId},
+      rating=${rating},
+      rating_count=${ratingCount},
+      review_count=${reviewCount},
+      description='${description}',
+      pages=${pages},
+      date_of_publication='${dateOfPublication}',
+      edition_language='${editionLanguage}',
+      price= ${price},
+      online_stores='${onlineStores}'
+    WHERE
+      book_id = ${bookId};`;
+
+  const updateBookResponse = await db.run(updateBookQuery);
+  response.send("simran book updated successfully");
+});
+
+//Delete Book API
+app.delete("/books/:bookId", async (request, response) => {
+  const { bookId } = request.params;
+  const deleteBookQuery = `
+        SELECT * 
+        FROM book 
+        WHERE book_id = ${bookId};`;
+
+  const deleteBook = await db.run(deleteBookQuery);
+  response.send("Book deleted Successfully!!!");
+});
+
+//Get Author Book API
+app.get("/authors/:authorId/books/", async (request, response) => {
+  const { authorId } = request.params;
+  const getAuthorBook = `
+     SELECT *
+     FROM book
+     WHERE author_id = ${authorId};
+    `;
+  const responseAuthorBook = await db.all(getAuthorBook);
+  response.send(responseAuthorBook);
+});
